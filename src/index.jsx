d0b986a10ed02c3e0e249ed0ff41104c02b8a4c7
Zlatko Fedor <zfedor@gmail.com>
2016-10-26 19:47:28 +0200

    Release 2.0.2

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 
 function createProvider(HighOrderComponent, ...args) {
-  return (Component) => {
+  return (Component, ...args2) => {
+    if (typeof Component !== 'function') {
+      const args3 = [Component, ...args2];
+      return (Component2) => {
+        function StateLessComponent2(props) {
+          return <HighOrderComponent originalProps={props} component={Component2} args={args3} />;
+        }
+
+        hoistNonReactStatics(StateLessComponent2, Component2);
+
+        return StateLessComponent2;
+      };
+    }
+
     function StateLessComponent(props) {
       return <HighOrderComponent originalProps={props} component={Component} args={args} />;
     }
